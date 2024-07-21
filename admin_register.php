1e48d4193c71c50f2026c429031cf5921c3e0dca
Nahidh <95267657+Nahidh96@users.noreply.github.com>
2024-07-21 12:21:22 +0530

    chore: Update admin_register.php title to include project name

--- admin_register.php
+++ admin_register.php
@@ -54,7 +54,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Register Admin</title>
+   <title>Register Admin | AgriConnect</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
