652ed119c41854d6e8f8602e292aaae2d54e8c2b
Nahidh <95267657+Nahidh96@users.noreply.github.com>
2024-07-21 12:22:59 +0530

    chore: Update page title to include project name

--- admin_update_product.php
+++ admin_update_product.php
@@ -60,7 +60,7 @@ if(isset($_POST['update_product'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>update products</title>
+   <title>Update Products | AgriConnect</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
