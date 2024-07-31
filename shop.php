b9751588d6981a56eec4fc4b5abb3dd29c089382
Nahidh Naseem <95267657+Nahidh96@users.noreply.github.com>
2024-07-31 15:38:20 +0530

    Update shop.php

--- shop.php
+++ shop.php
@@ -82,7 +82,7 @@ if(isset($_POST['add_to_cart'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>shop</title>
+   <title>Shop | AgriConnect</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
